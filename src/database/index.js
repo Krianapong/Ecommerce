883d00d933d5eda9e8e706c99c1a092979ef35e4
Krisanapong Jareunjanporn <150851052+Krianapong@users.noreply.github.com>
2023-12-27 00:28:01 +0700

    torror

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+  "mongodb+srv://krisanapongdt:123456782024@cluster0.qm7kral.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
